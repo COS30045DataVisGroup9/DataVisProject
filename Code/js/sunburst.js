98606f9d5042253736acaa771e32c8048117c069
nhmint19 <nguyenhoangminh191202@gmail.com>
2023-05-08 13:46:27 +1000

    sunburst add comments

--- Code/js/sunburst.js
+++ Code/js/sunburst.js
@@ -1,4 +1,5 @@
 function init() {
+  // partition
   partition = (data) =>
     d3.partition().size([2 * Math.PI, radius * radius])(
       d3
@@ -7,14 +8,16 @@ function init() {
         .sort((a, b) => b.value - a.value)
     );
 
+  // color scheme
   color = d3
     .scaleOrdinal()
     .domain(["home", "product", "search", "account", "other", "end"])
-    .range(["#5d85cf", "#7c6561", "#da7847", "#6fb971", "#9e70cf", "#bbbbbb"]);
+    .range(["#5d85cf", "#4E6E5D","#7c6561", "#da7847", "#6fb971", "#9e70cf", "#4D5057", "#bbbbbb"]);
 
   width = 700;
   radius = width / 2;
 
+  // create arc
   arc = d3
     .arc()
     .startAngle((d) => d.x0)
@@ -24,6 +27,7 @@ function init() {
     .innerRadius((d) => Math.sqrt(d.y0))
     .outerRadius((d) => Math.sqrt(d.y1) - 1);
 
+  // create arc with mouse events
   mousearc = d3
     .arc()
     .startAngle((d) => d.x0)
@@ -34,10 +38,17 @@ function init() {
   d3.json("../../DataVisProject/Data/Chart_1/visa2021_syd.json").then(function (data) {
     const root = partition(data);
     const svg = d3.select("#chart").append("svg");
-    // Make this into a view, so that the currently hovered sequence is available to the breadcrumb
+
+    // nodes of the svg
     const element = svg.node();
     element.value = { sequence: [], percentage: 0.0 };
 
+    svg
+      .attr("viewBox", `${-radius} ${-radius} ${width} ${width}`)
+      .style("max-width", `${width}px`)
+      .style("font", "12px sans-serif");
+
+    // labels
     const label = svg
       .append("text")
       .attr("text-anchor", "middle")
@@ -69,17 +80,13 @@ function init() {
       .attr("dy", "1.5em")
       .text("of total overseas migrant arrivals and departures");
 
-    svg
-      .attr("viewBox", `${-radius} ${-radius} ${width} ${width}`)
-      .style("max-width", `${width}px`)
-      .style("font", "12px sans-serif");
-
+    // path
     const path = svg
       .append("g")
       .selectAll("path")
       .data(
         root.descendants().filter((d) => {
-          // Don't draw the root node, and for efficiency, filter out nodes that would be too small to see
+          // filter out small nodes
           return d.depth && d.x1 - d.x0 > 0.001;
         })
       )
@@ -87,41 +94,44 @@ function init() {
       .attr("fill", (d) => color(d.data.name))
       .attr("d", arc);
 
+    // events
     svg
       .append("g")
       .attr("fill", "none")
       .attr("pointer-events", "all")
+      // mouse leave
       .on("mouseleave", () => {
         path.attr("fill-opacity", 1);
         label.style("visibility", "hidden");
-        // Update the value of this view
+        // update view value
         element.value = { sequence: [], percentage: 0.0 };
         element.dispatchEvent(new CustomEvent("input"));
       })
       .selectAll("path")
       .data(
         root.descendants().filter((d) => {
-          // Don't draw the root node, and for efficiency, filter out nodes that would be too small to see
+          // filter out small nodes
           return d.depth && d.x1 - d.x0 > 0.001;
         })
       )
       .join("path")
       .attr("d", mousearc)
       .on("mouseenter", (event, d) => {
-        // Get the ancestors of the current segment, minus the root
+        // get ancestors of the current segment eliminating root
         const sequence = d.ancestors().reverse().slice(1);
-        // Highlight the ancestors
+        // highlight ancestors
         path.attr("fill-opacity", (node) =>
           sequence.indexOf(node) >= 0 ? 1.0 : 0.3
         );
         const percentage = ((100 * d.value) / root.value).toPrecision(3);
+        // show labels
         label
           .style("visibility", null)
           .select(".percentage")
           .text(percentage + "%");
 
         label.select(".nodename").text(d.data.name);
-        // Update the value of this view with the currently hovered sequence and percentage
+        // update value
         element.value = { sequence, percentage };
         element.dispatchEvent(new CustomEvent("input"));
       });
